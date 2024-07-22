4de56459ed9039fea511986476fe52dd21f38785
rople277 <2727837474@qq.com>
2024-07-22 14:55:57 +0800

    sturct memory size

--- hello.c
+++ hello.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
+#include<string.h>
+struct student
+{
+    int id1;
+    int id2;
+    char a;
+    char b;
+    float percentage;
+
+};
+
 int main()
 {
-    printf("Hello World");
+    int i;
+    struct student record1 = {1, 2, 'A', 'B',90.9};
+
+    printf("size of structure in bytes : %d\n",sizeof(record1));
+
+    printf("\nAddress of id1      = %u",&record1.id1);
+    printf("\nAddress of id2      = %u",&record1.id2);
+    printf("\nAddress of a        = %u",&record1.a);
+    printf("\nAddress of b        = %u",&record1.b);
+    printf("\nAddress of percentage      = %u",&record1.percentage);
+
+    return 0;
 }
